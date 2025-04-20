c941a1e834bed8add15869709cdf0caafde66cee
Azmi FH <azmialhaq17@gmail.com>
2025-04-20 23:56:50 +0700

    tipe data

---
+++ tipe data.kt
@@ -0,0 +1,23 @@
+fun main(){
+    //integer (bilangan bulat)//
+
+    var age : Byte = 30
+    var height : Int = 170
+
+    //floating point number (.) adalah koma//
+    var value : Float = 10.11F //F adalah floa//
+
+    //literals (desimal)//
+    //underscore
+    var umur : Byte =  3_0
+
+    //conversion (penting)//
+    var number : Int = 1000
+    var byte : Byte =  number.toByte()
+    var int : Int = number.toInt()
+    var float : Float = number.toFloat()
+    print(float)
+
+
+}
+
