9dc02bb7f5dca298f1790bcc15aa008a15965764
Azmi FH <azmialhaq17@gmail.com>
2025-04-23 22:15:19 +0700

    tambah

---
+++ data range.kt
@@ -0,0 +1,10 @@
+fun main(){
+    //data untuk angka berurutan
+    val range = 0..100
+    println(range.contains(50))
+    println(range.count())
+    println(range.contains(200))
+    println(range.first)
+    println(range.last)
+    println(range.step)
+}
