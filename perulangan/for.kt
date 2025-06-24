655acf5c829bc9de2fbc23cb8e464b3bd512d5e0
Azmi FH <azmialhaq17@gmail.com>
2025-06-24 09:30:10 +0700

    perulangan dan percabangan

--- perulangan/for.kt
+++ perulangan/for.kt
@@ -1,5 +1,37 @@
 package perulangan
 
 fun main() {
+    val names = arrayOf("azmi ","fajarul ","haq")
+    for (name in names){
+        print(name)
+    }
+
+    for (i in 100 downTo 0 step 2){
+        println(i)
+    }
+
+
+//latihan 1
+    for (a in 0..10){
+        println(a)
+    }
+
+    // latihan 2
+    val daftarBuah = listOf("Apel", "Jeruk", "Mangga", "Anggur")
+    for (buah in daftarBuah){
+        println("aku suka makan buah "+buah)
+    }
+
+    //latihan3
+    for (b in 0..20 step 2){
+        println(b)
+    }
+
+    //latihan 4
+    for (c in 5 downTo 1){
+        println(c)
+    }
+    //latihan no 5 tidak bisa tolong berikan jawaban nya
 
 }
+
