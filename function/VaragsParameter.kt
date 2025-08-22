1c04e3f716b214d228230bfb12912897ae2fbd2b
Azmi FH <azmialhaq17@gmail.com>
2025-08-23 03:22:55 +0700

    baru

---
+++ function/VaragsParameter.kt
@@ -0,0 +1,18 @@
+package function
+
+fun hitungTotal(name: String, vararg values: Int): Int {
+    var total = 0
+
+    for (value in values) {
+        total += value
+    }
+
+    return total;
+}
+
+fun main() {
+    // val values = arrayOf(10,10,10,10)
+    val result = hitungTotal("Eko", 10, 10, 10)
+
+    println(result)
+}
