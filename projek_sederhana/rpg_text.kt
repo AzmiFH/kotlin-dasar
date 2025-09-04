ebfab9286beac907e25665feeb913d8ec3250501
Azmi Fajarul Haq <azmialhaq17@gmail.com>
2025-09-04 09:01:46 +0700

    projek

---
+++ projek_sederhana/rpg_text.kt
@@ -0,0 +1,7 @@
+package projek_sederhana
+
+fun main(){
+
+    print("Masukkan angka pertama: ")
+    val angka1 = readLine()?.toDoubleOrNull() ?: 0.0
+}
---
+++ tipe_dataNumber.kt
@@ -0,0 +1,30 @@
+fun miniprojek() {
+
+
+}
+fun main() {
+    val panjang : Int = 10
+    val lebar : Int = 50
+    var hasil = panjang*lebar
+    //var age: Int = 200
+
+    /** var sample: Float = 10.11F
+
+    println(age)
+    println(sample)
+
+    var binary: Int = 0b010101010
+    println(binary)
+
+    var price: Long = 9_000_000_000L;
+    println(price)
+
+    var priceInt: Int = price.toInt()
+    println(priceInt)
+
+    var doubleBinary: Double = binary.toDouble()
+    println(doubleBinary)**/
+
+    println("===========mini projek =============")
+    println(hasil)
+}
