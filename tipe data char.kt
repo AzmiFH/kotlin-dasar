ba198b9a697b3de6b974c381000c6618fc173096
Azmi Fajarul Haq <azmialhaq17@gmail.com>
2025-09-15 18:26:09 +0700

    projek

--- tipe data char.kt
+++ tipe data char.kt
@@ -2,3 +2,14 @@ fun main(){
     var char = "A"
     var char_angka = 1
 }
+/* Tentu, ini catatannya.
+
+Catatan Singkat: Tipe Data Dasar Kotlin
+Char (Karakter)
+Tujuan: Menyimpan satu karakter saja.
+
+Aturan Wajib: Harus diapit tanda petik satu (' ').
+
+Contoh: 'A', 'z', '7', '@'.
+
+Ingat: Petik satu (' ') untuk satu karakter, petik dua (" ") untuk banyak karakter (String).*/
