d705cb8acb16cc4a25cf3a48acd9de862030eb4c
Azmi Fajarul Haq <azmialhaq17@gmail.com>
2025-09-15 19:52:11 +0700

    projek

--- tipe String.kt
+++ tipe String.kt
@@ -15,6 +15,48 @@ fun main(){
 
     //string template dengan tanda dollar
     //println("$nama$nama2 haq")
+/*Tentu, ini dia catatan ringkasnya.
 
+## String Biasa
+Tanda: Petik dua (" ").
+
+Ingat: Untuk teks satu baris.
+
+## Multiline String
+Tanda: Petik dua tiga kali (""" """).
+
+Ingat: Untuk teks banyak baris (seperti alamat atau puisi).
+
+## Escaped String
+Tanda: Backslash (\).
+
+Ingat: "Sinyal khusus" sebelum karakter spesial.
+
+\n → new line (baris baru).
+
+\t → tab.
+
+\" → cetak tanda ".
+
+\\ → cetak tanda ****.
+
+## String Template
+Tanda: Dolar ($).
+
+Ingat: Cara menyisipkan variabel ke dalam teks.
+
+$nama → untuk variabel biasa.
+
+${ ekspresi } → untuk operasi/ekspresi yang lebih rumit.
+
+## trimIndent()
+Kata Kunci: Indentasi.
+
+Ingat: Merapikan spasi secara otomatis dan "pintar".
+
+## trimMargin()
+Kata Kunci: Margin (Batas).
+
+Ingat: Merapikan spasi secara manual dengan penanda |*/
 
 }
